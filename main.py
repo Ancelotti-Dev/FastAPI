93122d50405280f4f8c90dcad4a4e1a28e2e9818
Ancelotti-Dev <pedroh.moliveira20@gmail.com>
2025-05-08 21:15:52 -0300

    Resolvendo problema de conflito de informações dentro do body da requisição

--- main.py
+++ main.py
@@ -1,35 +1,43 @@
 from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 
 app = FastAPI()
 
-tarefas = {}
+Dict_tarefinhas = {}
+
+class Tarefa(BaseModel):
+    nome: str
+    descricao: str
+    concluida: bool = False
 
 @app.get("/")
 def read_root():
-    return {"message": "ANCELA API de Tarefas"}
+    return {"message": "ANCELA API de Dict_tarefinhas"}
 
-@app.get('/tarefas')
-def get_tarefas():
-    return tarefas
+@app.get("/tarefinhas")
+def get_Dict_tarefinhas():
+    if not Dict_tarefinhas:
+        return {"message": "Não há nenhuma tarefa ainda"}
+    return Dict_tarefinhas
 
-@app.post("/adiciona")  
-def add_tarefa(nome: str, descricao: str):
-    if nome in tarefas:
+@app.post("/adiciona")
+def add_tarefa(tarefa: Tarefa):
+    if tarefa.nome in Dict_tarefinhas:
         raise HTTPException(status_code=400, detail="Tarefa já existe")
-    tarefas[nome] = {"descricao": descricao, "concluida": False}
-    return {"message": "Tarefa adicionada com sucesso", "tarefa": tarefas[nome]}
+    Dict_tarefinhas[tarefa.nome] = tarefa.dict()
+    return {"message": "Tarefa adicionada com sucesso"}
 
 @app.put("/concluida/{nome}")
-def tarefa_concluida(nome: str, concluida: bool = True):
-    if nome not in tarefas:
+def tarefa_concluida(nome: str):
+    if nome not in Dict_tarefinhas:
         raise HTTPException(status_code=404, detail="Tarefa não encontrada")
-    tarefas[nome]["concluida"] = concluida
-    return {"message": "Status da tarefa atualizado", "tarefa": tarefas[nome]}
+    Dict_tarefinhas[nome]["concluida"] = True
+    return {"message": "Tarefa marcada como concluída", "tarefa": Dict_tarefinhas[nome]}
 
 @app.delete("/delete/{nome}")
 def delete_tarefa(nome: str):
-    if nome in tarefas:
-        del tarefas[nome]
-        return {"message": "Tarefa excluida com sucesso!"}
+    if nome in Dict_tarefinhas:
+        del Dict_tarefinhas[nome]
+        return {"message": "Tarefa excluída com sucesso!"}
     else:
-        raise HTTPException(status_code=404, detail="Tarefa não encontarada")
+        raise HTTPException(status_code=404, detail="Tarefa não encontrada")
