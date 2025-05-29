dab4480bafa453537f92c3e5823783f9f8e145ee
Ancelotti-Dev <pedroh.moliveira20@gmail.com>
2025-05-29 13:28:24 -0300

    Paginação

--- main.py
+++ main.py
@@ -18,7 +18,6 @@ class Tarefa(BaseModel):
     descricao: str
     concluida: bool = False
 
-
 def autenticate_user(credentials: HTTPBasicCredentials = Depends(security)):
     is_username_correct = secrets.compare_digest(credentials.username, My_user)
     is_password_correct = secrets.compare_digest(credentials.password, My_password)
@@ -36,11 +35,30 @@ def read_root():
     return {"message": "Tarefas"}
 
 @app.get("/tarefinhas")
-def get_tarefinhas(credentials: HTTPBasicCredentials = Depends(autenticate_user)):
+def get_tarefinhas(page: int=1, limit: int=10, credentials: HTTPBasicCredentials = Depends(autenticate_user)):
+    
+    if page < 1 or limit< 1:
+        raise HTTPException(status_code=400, detail="Não é possível acessar paginas menores que 0")
+
     if not Dict_tarefinhas:
         return { "detail": "Não há tarefas cadastradas"}
-    else:
-        return Dict_tarefinhas
+    
+
+    start = (page - 1) * limit
+    end = start + limit
+    
+    
+    tarefinhas_paginadas = [
+        {"nome": nome , "descricao": tarefa["descricao"], "concluida": tarefa["concluida"]}
+        for nome, tarefa in list(Dict_tarefinhas.items())[start:end]
+    ]
+
+    return {
+        "page": page,
+        "limit": limit,
+        "tarefinhas": tarefinhas_paginadas,
+        "total": len(Dict_tarefinhas)
+    }
 
 @app.post("/adiciona")
 def add_tarefa(tarefa: Tarefa , credentials: HTTPBasicCredentials = Depends(autenticate_user)):
